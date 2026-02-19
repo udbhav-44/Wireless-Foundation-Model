294bd71d4f76789614c4fcfe4b8d53065b304930
Udbhav Agarwal <audbhav22@iitk.ac.in>
2026-02-19 16:04:59 +0530

    Implemented RoPe, untied decoder, added residual connections

---
+++ lwm_axial/rope.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+
+class RotaryEmbedding(nn.Module):
+    def __init__(self, dim, max_position_embeddings=2048, base=10000, device=None):
+        super().__init__()
+        inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2).float().to(device) / dim))
+        self.register_buffer("inv_freq", inv_freq)
+        self.max_seq_len_cached = max_position_embeddings
+        t = torch.arange(self.max_seq_len_cached, device=device, dtype=self.inv_freq.dtype)
+        freqs = torch.einsum("i,j->ij", t, self.inv_freq)
+        # Different from paper, but it uses a different permutation in order to obtain the same calculation
+        emb = torch.cat((freqs, freqs), dim=-1)
+        self.register_buffer("cos_cached", emb.cos()[None, None, :, :], persistent=False)
+        self.register_buffer("sin_cached", emb.sin()[None, None, :, :], persistent=False)
+
+    def forward(self, x, seq_len=None):
+        # x: [batch, n_heads, seq_len, head_dim]
+        if seq_len > self.max_seq_len_cached:
+            self.max_seq_len_cached = seq_len
+            t = torch.arange(self.max_seq_len_cached, device=x.device, dtype=self.inv_freq.dtype)
+            freqs = torch.einsum("i,j->ij", t, self.inv_freq)
+            emb = torch.cat((freqs, freqs), dim=-1)
+            self.register_buffer("cos_cached", emb.cos()[None, None, :, :], persistent=False)
+            self.register_buffer("sin_cached", emb.sin()[None, None, :, :], persistent=False)
+        return (
+            self.cos_cached[:, :, :seq_len, ...].to(dtype=x.dtype),
+            self.sin_cached[:, :, :seq_len, ...].to(dtype=x.dtype),
+        )
+
+def rotate_half(x):
+    """Rotates half the hidden dims of the input."""
+    x1 = x[..., : x.shape[-1] // 2]
+    x2 = x[..., x.shape[-1] // 2 :]
+    return torch.cat((-x2, x1), dim=-1)
+
+def apply_rotary_pos_emb(q, k, cos, sin, position_ids=None):
+    """
+    Applies Rotary Position Embedding to the query and key tensors.
+    """
+    # q, k: [batch, n_heads, seq_len, head_dim]
+    # cos, sin: [1, 1, seq_len, head_dim]
+    
+    # If position_ids are provided, gather the correct cos/sin
+    if position_ids is not None:
+        # position_ids: [batch, seq_len]
+        # visible_cos: [batch, 1, seq_len, head_dim]
+        cos = cos.squeeze(1).squeeze(0) # [max_len, dim]
+        sin = sin.squeeze(1).squeeze(0)
+        
+        cos = cos[position_ids].unsqueeze(1) # [batch, 1, seq_len, dim]
+        sin = sin[position_ids].unsqueeze(1)
+
+    q_embed = (q * cos) + (rotate_half(q) * sin)
+    k_embed = (k * cos) + (rotate_half(k) * sin)
+    return q_embed, k_embed
